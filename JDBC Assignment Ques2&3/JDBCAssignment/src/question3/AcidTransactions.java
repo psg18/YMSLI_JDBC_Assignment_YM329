16a24b4b8849a30d08c8cb6a724c8fe9b85323be
psg18 <72650325+psg18@users.noreply.github.com>
2022-01-29 17:14:53 +0530

    Update AcidTransactions.java

--- JDBC Assignment Ques2&3/JDBCAssignment/src/question3/AcidTransactions.java
+++ JDBC Assignment Ques2&3/JDBCAssignment/src/question3/AcidTransactions.java
@@ -13,19 +13,17 @@ public class AcidTransactions {
 	
 	public static void main(String[] args) throws SQLException{
 
-		Connection connection = ConnectionFactory.getConnection();
-
 		try {
 
 			connection.setAutoCommit(false);
 			
 			connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 			
-			PreparedStatement pstmt = connection.prepareStatement("update account set balance=balance-10 where id=?");
+			PreparedStatement pstmt = connection.prepareStatement("update account set balance=balance-1000 where id=?");
 			pstmt.setInt(1, 1);
 			pstmt.executeUpdate();
 
-			pstmt = connection.prepareStatement("update account set balance=balance+10 where id=?");
+			pstmt = connection.prepareStatement("update account set balance=balance+1000 where id=?");
 			pstmt.setInt(1, 2);
 			pstmt.executeUpdate();
 
